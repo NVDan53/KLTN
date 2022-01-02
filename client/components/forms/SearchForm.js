9544e5e2f794c3cd7169ce56f0a1f90b62ec13b0
dan <nguyenvandan0503@gmail.com>
2022-01-02 12:53:26 +0700

    complete handling 2 line title on course card and show hidden search results

--- client/components/forms/SearchForm.js
+++ client/components/forms/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import { Input } from "antd";
 import Link from "next/link";
@@ -22,32 +22,44 @@ function SearchForm() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
   const [load, setLoad] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+
   const typingTimeoutRef = useRef(null);
 
   const { courses, posts } = data;
 
+  const handleResults = (e) => {
+    if (e.target.tagName === "INPUT") return;
+    setShowResults(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleResults);
+
+    return () => {
+      document.removeEventListener("click", handleResults);
+    };
+  }, []);
+
   const handleSearch = async (value, e) => {
     e.preventDefault();
 
-    console.log("VALUE", value);
     if (!value || value.search === "") return;
     try {
       setLoad(true);
+      setShowResults(true);
       const res = await axios.get(`/api/search?name=${value.search}`);
-      console.log("SEARCH RESULT:", res);
       setData(res.data);
       setLoad(false);
     } catch (err) {
       setLoad(false);
-      console.log("ERRROR:", err);
+      setShowResults(false);
     }
   };
 
   const handleOnChange = async (e) => {
     const value = e.target.value;
     setSearch(value);
-
-    // if (!search) return;
 
     if (typingTimeoutRef.current) {
       clearTimeout(typingTimeoutRef.current);
@@ -59,85 +71,99 @@ function SearchForm() {
       };
 
       handleSearch(formValues, e);
-    }, 600);
+    }, 400);
   };
 
   return (
     <form style={{ position: "relative" }} autoComplete="off">
-      <Input placeholder="Search courses and blogs" loading={load}
+      <Input
+        placeholder="Search courses and blogs"
+        loading={load}
         onChange={handleOnChange}
-        onSearch={handleSearch} 
-        style={{marginLeft:"70px",width:"400px",borderRadius:"50px",marginTop:"5px",marginBottom:"5px"}}
-        prefix={<i class="fas fa-search mr-2 text-gray-400"></i>}
-        />
-      <ul
-        // className="border border-gray-900"
+        onSearch={handleSearch}
+        onFocus={() => setShowResults(true)}
         style={{
-          position: "absolute",
-          zIndex: "99",
-          color:"#333 !important",
-          width: "100%",
-          minWidth: "250px",
-          maxHeight: "calc(100vh - 150px)",
-          overflowY: "hidden",
-          overflowX: "hidden",
-          marginTop: "6px",
-          background: "#fff",
-          marginLeft:"70px",
-          // padding: "0 11px",
-          // border:"1px solid #333"
-          // boxShadow:"0 0 7px 4px #33333326",
+          marginLeft: "70px",
+          width: "400px",
+          borderRadius: "50px",
+          marginTop: "5px",
+          marginBottom: "5px",
         }}
-      >
-        {search &&
-          courses?.map((course) => {
-            return (
-              <li
-                style={{
-                  overflow: "hidden",
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
-                  textOverflow: "ellipsis",
-                  WebkitLineClamp: "1",
-                  padding: "0 11px",
-                }}
-              >
-                <Link href={`/course/${course.slug}`}>
-                  <a
-                    style={{
-                      display: "block",
-                    }}
-                    className="reset-before text-dark"
-                  ><i class="fas fa-chalkboard-teacher mr-2 text-gray-400"></i>
-                    {course.name}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
+        prefix={<i class="fas fa-search mr-2 text-gray-400"></i>}
+      />
 
-        {search &&
-          posts?.map((post) => {
-            return (
-              <li
-                style={{
-                  overflow: "hidden",
-                  display: "-webkit-box",
-                  WebkitBoxOrient: "vertical",
-                  textOverflow: "ellipsis",
-                  WebkitLineClamp: "1",
-                  padding: "0 11px",
-                }}
-              >
-                <Link href={`/article/${post.slug}`}>
-                  <a className="reset-before  text-dark">
-                    <i class="far fa-file-alt mr-2 text-gray-400"></i>{post.title}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-      </ul>
+      {showResults && (
+        <ul
+          // className="border border-gray-900"
+          style={{
+            position: "absolute",
+            zIndex: "99",
+            color: "#333 !important",
+            width: "100%",
+            minWidth: "250px",
+            maxHeight: "calc(100vh - 150px)",
+            overflowY: "hidden",
+            overflowX: "hidden",
+            marginTop: "6px",
+            background: "#fff",
+            marginLeft: "70px",
+            // padding: "0 11px",
+            // border:"1px solid #333"
+            // boxShadow:"0 0 7px 4px #33333326",
+          }}
+        >
+          {search &&
+            courses?.map((course) => {
+              return (
+                <li
+                  style={{
+                    overflow: "hidden",
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    textOverflow: "ellipsis",
+                    WebkitLineClamp: "1",
+                    padding: "0 11px",
+                  }}
+                >
+                  <Link href={`/course/${course.slug}`}>
+                    <a
+                      style={{
+                        display: "block",
+                      }}
+                      className="reset-before text-dark"
+                    >
+                      <i className="fas fa-chalkboard-teacher mr-2 text-gray-400"></i>
+                      {course.name}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+
+          {search &&
+            posts?.map((post) => {
+              return (
+                <li
+                  style={{
+                    overflow: "hidden",
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    textOverflow: "ellipsis",
+                    WebkitLineClamp: "1",
+                    padding: "0 11px",
+                  }}
+                >
+                  <Link href={`/article/${post.slug}`}>
+                    <a className="reset-before  text-dark">
+                      <i className="far fa-file-alt mr-2 text-gray-400"></i>
+                      {post.title}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+        </ul>
+      )}
     </form>
   );
 }
