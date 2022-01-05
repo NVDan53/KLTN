078ac637336a5c8681685a03fdac4fceb82e1615
OliverDoan <doanvietthuan1005@gmail.com>
2022-01-05 13:24:49 +0700

    fix page user nav

--- client/components/nav/UserNav.js
+++ client/components/nav/UserNav.js
@@ -15,37 +15,57 @@ const UserNav = () => {
 
   return (
     <>
-    <div className="bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0" style={{height:"100%",marginLeft:"-15px"}}>
-    <div className="flex items-center justify-center mt-8">
-     <div className="flex items-center">
-       <span className="text-white text-2xl mx-2 font-semibold">Menu</span>
-     </div>
-    </div>
-    <nav className="mt-10">
-    <Link href="/user">
-            <a className={`nav-link ${current === "/user" && "active"}`}className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-            <i class="fas fa-tachometer-alt h-6 w-6" style={{fontSize:"24px"}}></i>
-          <span className="mx-3">Dashboard</span>
-        </a>
-        </Link>
-        <Link href="/user/support">
-            <a className={`nav-link ${current === "/user/support" && "active"}`}className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-            <i class="fas fa-headset h-6 w-6" style={{fontSize:"24px"}}></i>
-           
-          <span className="mx-3">Help</span>
-        </a>
-        </Link>
-        {user && user.courses && user.courses.length >= 1 && (
-          <Link href="/user/qa">
-          <a className={`nav-link ${current === "/user/qa" && "active"}`} className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-          <i class="far fa-question-circle h-6 w-6" style={{fontSize:"24px"}}></i>
-          <span className="mx-3">Question</span>
-        </a>
-        </Link>
-        )}
-      </nav>
-    </div>
- 
+      <div
+        className="min-h-screen bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0"
+        style={{ height: "100%", marginLeft: "-15px" }}
+      >
+        <div className="flex items-center justify-center mt-8">
+          <div className="flex items-center">
+            <span className="text-white text-2xl mx-2 font-semibold">Menu</span>
+          </div>
+        </div>
+        <nav className="mt-10">
+          <Link href="/user">
+            <a
+              className={`nav-link ${current === "/user" && "active"}`}
+              className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+            >
+              <i
+                class="fas fa-tachometer-alt h-6 w-6"
+                style={{ fontSize: "24px" }}
+              ></i>
+              <span className="mx-3">Dashboard</span>
+            </a>
+          </Link>
+          <Link href="/user/support">
+            <a
+              className={`nav-link ${current === "/user/support" && "active"}`}
+              className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+            >
+              <i
+                class="fas fa-headset h-6 w-6"
+                style={{ fontSize: "24px" }}
+              ></i>
+
+              <span className="mx-3">Help</span>
+            </a>
+          </Link>
+          {user && user.courses && user.courses.length >= 1 && (
+            <Link href="/user/qa">
+              <a
+                className={`nav-link ${current === "/user/qa" && "active"}`}
+                className="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+              >
+                <i
+                  class="far fa-question-circle h-6 w-6"
+                  style={{ fontSize: "24px" }}
+                ></i>
+                <span className="mx-3">Question</span>
+              </a>
+            </Link>
+          )}
+        </nav>
+      </div>
     </>
   );
 };
