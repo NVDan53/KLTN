2ebe9610b618e13be9a1ebdaf97a8eece9b5a3a9
OliverDoan <doanvietthuan1005@gmail.com>
2022-01-05 13:27:04 +0700

    fix FE page issues of admin

--- client/pages/admin/issues/index.js
+++ client/pages/admin/issues/index.js
@@ -66,101 +66,111 @@ const AdminIssuesIndex = () => {
 
   return (
     <AdminRoute>
-         <div className="text-blue-900 text-sm rounded-md"style={{margin:"16px"}}>
+      <div
+        className="text-blue-900 text-sm rounded-md"
+        style={{ margin: "16px" }}
+      >
         <ul className="flex">
-          <li><a href="/admin" className="underline font-semibold">Dashboard</a></li>
-          <li><span className="mx-2">/</span></li>  
-          <li>List issues</li>
+          <li>
+            <a href="/instructor" className="underline font-semibold">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <span className="mx-2">/</span>
+          </li>
+          <li>List categories</li>
         </ul>
       </div>
-
-
-      <div className="inline-block w-full shadow rounded-lg overflow-hidden mb-4">
-     <table className="w-full bg-white leading-normal">
-       <thead>
-         <tr>
-           <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-             Link course
-           </th>
-           <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-             Message
-           </th>
-           <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-            Email
-           </th>
-           <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-           Created at
-           </th>
-           <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-           Action
-           </th>
-         </tr>
-       </thead>
-       <tbody>
-       {issues.map((issue) => (
-            
-         <tr key={issue._id}  className="xyz hover:bg-sky-100">
-          
-           <td className="px-4 py-4 border-b border-gray-200  text-sm">
-             <p className="text-gray-900 whitespace-no-wrap font-semibold"> {issue.course_url ? (
-                  <Link href={issue.course_url}>
-                    <a target="_blank">{issue.course_url}</a>
-                  </Link>
-                ) : (
-                  "No URL"
-                )}</p>
-           </td>
-           <td className="px-4 py-4 border-b border-gray-200  text-sm">
-             <p className="text-gray-900 whitespace-no-wrap font-semibold"style={{width:"430px"}}>{issue.message}</p>
-           </td>
-           <td className="px-4 py-4 border-b border-gray-200   text-sm">
-             <p className="text-gray-900 whitespace-no-wrap">
-             {issue.postedBy.email}
-             </p>
-           </td>
-           <td className="px-4 py-4 border-b border-gray-200   text-sm">
-             <p className="text-gray-900 whitespace-no-wrap">
-             {new Date(issue.createdAt).toLocaleDateString()}
-             </p>
-           </td>
-           <td className="px-4 py-4 border-b border-gray-200  text-sm">
-             <p className="text-gray-900 whitespace-no-wrap font-semibold">  {issue.resolved ? (
-                    <Tooltip title="Resolved">
-                      {loading ? (
-                        <LoadingOutlined className="text-success h4" />
+      <div>
+        <div className="inline-block w-full shadow rounded-lg overflow-hidden mb-4 mt-4">
+          <table className="w-full bg-white leading-normal">
+            <thead>
+              <tr>
+                <th className="pl-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Link course
+                </th>
+                <th className="pl-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Message
+                </th>{" "}
+                <th className="pl-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Email
+                </th>{" "}
+                <th className="pl-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Created at
+                </th>{" "}
+                <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {issues.map((issue) => (
+                <tr key={issue._id} className="xyz hover:bg-sky-100">
+                  <td className="pl-4 py-4 border-b border-gray-200  text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap font-semibold">
+                      {issue.course_url ? (
+                        <Link href={issue.course_url}>
+                          <a target="_blank">{issue.course_url}</a>
+                        </Link>
                       ) : (
-                        <CheckOutlined className="text-success h4" />
+                        "No URL"
                       )}
-                    </Tooltip>
-                  ) : (
-                    <Tooltip title="Not resolved">
-                      {loading ? (
-                        <LoadingOutlined className="text-danger h4" />
-                      ) : (
-                        <WarningOutlined
-                          onClick={() => handleEnrollmentIssue(issue)}
-                          className="text-danger h4 pointer"
-                        />
-                      )}
-                    </Tooltip>
-                  )}
+                    </p>
+                  </td>
+                  <td
+                    className="pl-4 py-4 border-b border-gray-200  text-sm"
+                    style={{ width: "500px" }}
+                  >
+                    <p className="text-gray-900 whitespace-no-wrap font-semibold">
+                      {issue.message}
+                    </p>
+                  </td>
+                  <td className="pl-4 py-4 border-b border-gray-200  text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap font-semibold">
+                      {issue.postedBy.email}
+                    </p>
+                  </td>
+                  <td className="pl-4 py-4 border-b border-gray-200  text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap font-semibold">
+                      {new Date(issue.createdAt).toLocaleDateString()}
+                    </p>
+                  </td>
+                  <td className="px-4 border-b border-gray-200  text-sm ">
+                    {issue.resolved ? (
+                      <Tooltip title="Resolved">
+                        {loading ? (
+                          <LoadingOutlined className="text-success h4 pr-2" />
+                        ) : (
+                          <CheckOutlined className="text-success h4 pr-2" />
+                        )}
+                      </Tooltip>
+                    ) : (
+                      <Tooltip title="Not resolved">
+                        {loading ? (
+                          <LoadingOutlined className="text-danger h4 pr-2" />
+                        ) : (
+                          <WarningOutlined
+                            onClick={() => handleEnrollmentIssue(issue)}
+                            className="text-danger h4 pointer pr-2"
+                          />
+                        )}
+                      </Tooltip>
+                    )}
 
-                  <Tooltip title="Delete">
-                    <DeleteOutlined
-                      onClick={() => deleteIssue(issue._id)}
-                      className="text-danger h4"
-                    />
-                  </Tooltip></p>
-           </td>
-         </tr>
-        
-         ))}
-       </tbody>
-     </table>
-     
-  
- 
-</div>
+                    <Tooltip title="Delete">
+                      <DeleteOutlined
+                        onClick={() => deleteIssue(issue._id)}
+                        className="text-danger h4"
+                      />
+                    </Tooltip>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </AdminRoute>
   );
 };
